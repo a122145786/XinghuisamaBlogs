65dbd9e8cb299933cba730faea1052f799cd04d7
a122145786 <122145786@qq.com>
2026-09-13 14:24:04 +0800

    feat: 恢复首页文章/照片墙/说说三个卡片

--- my-blog-manager/data/albums.ts
+++ my-blog-manager/data/albums.ts
@@ -2,4 +2,17 @@
 export interface Photo { url: string; caption?: string; }
 export interface Album { id: string; title: string; description: string; cover: string; date: string; photos: Photo[]; }
 
-export const albums: Album[] = [];
+export const albums: Album[] = [
+  {
+    "title": "11",
+    "description": "11",
+    "id": "album_1789280289972",
+    "photos": [
+      {
+        "caption": "https://cdn.a1221.cc/1/%E6%AF%8F%E6%97%A5%E5%A4%B4%E5%83%8F_1_%E5%86%B0%E5%9D%97%E5%B0%8F%E6%9D%B0%F0%9F%8E%AB_%E6%9D%A5%E8%87%AA%E5%B0%8F%E7%BA%A2%E4%B9%A6%E7%BD%91%E9%A1%B5%E7%8"
+      }
+    ],
+    "date": "2026-09-13",
+    "cover": "https://cdn.a1221.cc/1/%E6%AF%8F%E6%97%A5%E5%A4%B4%E5%83%8F_1_%E5%86%B0%E5%9D%97%E5%B0%8F%E6%9D%B0%F0%9F%8E%AB_%E6%9D%A5%E8%87%AA%E5%B0%8F%E7%BA%A2%E4%B9%A6%E7%BD%91%E9%A1%B5%E7%8"
+  }
+];
